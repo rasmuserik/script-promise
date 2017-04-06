32135717857e8777ef6c0be85e84c7940011e041
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-04-06 16:46:49 +0200

    initial implementation

--- script-promise.js
+++ script-promise.js
@@ -1,6 +1,13 @@
-// # JavaScript script-promise
+// # Script promis
 //
-// Template for new solsort projects.
+// Small utility function for loading code through a script tag,
+// and returning a promise that will be fulfilled when the
+// code is loaded.
 //
-// Includes minimal webpack config, test-runner, etc.
-//
+export default (src) => new Promise((resolve, reject) => {
+  let elem = document.createElement('script');
+  elem.src = src;
+  elem.onload = resolve;
+  elem.onerror = reject;
+  document.head.appendChild(elem);
+});
