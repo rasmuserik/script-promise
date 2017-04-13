c5b5460e3efd17dc2e4ac528af362bd093655b51
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-04-13 07:43:45 +0200

    commonjs-module

--- script-promise.js
+++ script-promise.js
@@ -4,7 +4,7 @@
 // and returning a promise that will be fulfilled when the
 // code is loaded.
 //
-export default (src) => new Promise((resolve, reject) => {
+exports.scriptPromise = (src) => new Promise((resolve, reject) => {
   let elem = document.createElement('script');
   elem.src = src;
   elem.onload = resolve;
